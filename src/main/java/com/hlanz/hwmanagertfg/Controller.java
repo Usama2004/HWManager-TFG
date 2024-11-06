d4e9b247f1bdb59e619750e2f5efe1803b19c2bd
oeddahab@emeal.nttdata.com <oeddahab@emeal.nttdata.com>
2024-11-06 09:01:19 +0100

    Prueba para ver que los cambios se aplican

--- src/main/java/com/hlanz/hwmanagertfg/Controller.java
+++ src/main/java/com/hlanz/hwmanagertfg/Controller.java
@@ -3,7 +3,7 @@ package com.hlanz.hwmanagertfg;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
